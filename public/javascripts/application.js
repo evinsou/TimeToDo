f88c160d2fd74c8065b57d94377deea921270a4c
Ivan <evinsou@gmail.com>
2012-01-19 17:44:56 +0200

    add clock to header

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -30,5 +30,29 @@
     $('#sortTabs').tabs();
   });
 
+$(document).ready(function() {
 
+  var newDate = new Date();
+
+setInterval( function() {
+	// Create a newDate() object and extract the seconds of the current time on the visitor's
+	var seconds = new Date().getSeconds();
+	// Add a leading zero to seconds value
+	$("#sec").html(( seconds < 10 ? "0" : "" ) + seconds);
+	},1000);
+	
+setInterval( function() {
+	// Create a newDate() object and extract the minutes of the current time on the visitor's
+	var minutes = new Date().getMinutes();
+	// Add a leading zero to the minutes value
+	$("#min").html(( minutes < 10 ? "0" : "" ) + minutes);
+    },1000);
+	
+setInterval( function() {
+	// Create a newDate() object and extract the hours of the current time on the visitor's
+	var hours = new Date().getHours();
+	// Add a leading zero to the hours value
+	$("#hours").html(( hours < 10 ? "0" : "" ) + hours);
+    }, 1000);	
+});
 
